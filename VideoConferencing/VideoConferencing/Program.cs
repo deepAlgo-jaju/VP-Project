0ba4e8c8116c7ba9a99e2f59e4957001af076772
deepAlgo-jaju <muhammadjunaidiftikhar@gmail.com>
2015-05-24 00:53:02 +0500

    a little success :)

--- VideoConferencing/VideoConferencing/Program.cs
+++ VideoConferencing/VideoConferencing/Program.cs
@@ -16,6 +16,7 @@ namespace VideoConferencing
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new P2P());
             Application.Run(new Login_Form());
             //Application.Run(new Main_Form());
             //Application.Run(new PracticeForm());
